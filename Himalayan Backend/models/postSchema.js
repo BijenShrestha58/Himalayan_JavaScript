9ace9f50266e08d7f1ead523eb83e8c42776b28f
Aryanlivi <aryanlivi@gmail.com>
2023-07-19 22:53:21 +0545

    testing postSchema

--- Himalayan Backend/models/postSchema.js
+++ Himalayan Backend/models/postSchema.js
@@ -1,9 +1,9 @@
 const mongoose=require('mongoose');
 
-const Post= mongoose.model('Post',new mongoose.Schema({
+const Post= new mongoose.Schema({
     content:{type:String,required:true,trim:true},
     postedBy:{type:mongoose.Schema.Types.ObjectId,ref:'User'},
     comments:{type:mongoose.Schema.Types.ObjectId,ref:"Comment"},
     upVotes:Number
-},{timestamps:true}))
-exports.post = Post;
+},{timestamps:true})
+module.exports=Post;
--- Himalayan Backend/routes/userRoutes.js
+++ Himalayan Backend/routes/userRoutes.js
@@ -2,7 +2,7 @@ const express=require("express");
 const router=express.Router();
 const {User}=require("../models/userSchema");
 const {getUsers,getUser,addUser} = require('../controllers/userController')
-const Official=require("../models/officialSchema");
+const {Official}=require("../models/officialSchema");
 
 //Get
 router.get('/',getUsers)
