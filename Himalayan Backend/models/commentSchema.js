fc09489a1a6b6320b16bf4bc1ef0820302810bf7
Aryanlivi <aryanlivi@gmail.com>
2023-07-20 01:21:44 +0545

    testing commentSchema

--- Himalayan Backend/models/commentSchema.js
+++ Himalayan Backend/models/commentSchema.js
@@ -2,7 +2,8 @@ const mongoose=require('mongoose');
 
 const commentSchema= new mongoose.Schema({
     content:{type:String,required:true,trim:true},
-    postedBy:{type:mongoose.Schema.Types.ObjectId,ref:'User'},
+    author:{type:mongoose.Schema.Types.ObjectId,ref:'User'},
+    postId:{type:mongoose.Schema.Types.ObjectId,ref:"Post"},
     upVotes:Number
 },{timestamps:true})
 const Comment=mongoose.model("Comment",commentSchema)
