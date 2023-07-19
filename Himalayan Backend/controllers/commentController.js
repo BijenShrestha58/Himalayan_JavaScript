4f6e55d2c7dde90f8d28f77f55a8b67b06c15bda
Aryanlivi <aryanlivi@gmail.com>
2023-07-20 02:45:39 +0545

    update user db on post and comments

--- Himalayan Backend/controllers/commentController.js
+++ Himalayan Backend/controllers/commentController.js
@@ -1,10 +1,14 @@
 const Joi = require('joi')
 const {Comment} = require('../models/commentSchema')
-
+const {Post}=require("../models/postSchema");
 const createComment =async (req,res)=>{
     try{
         let comment = new Comment(req.body)
         comment = await comment.save()
+        await Post.findById(comment.postId).then(user=>{
+            user.comments.push(comment.postId);
+            return user.save();
+        }).catch(err=>{ res.status(500).json({message:err.message});})
         res.send(comment)
     }catch(error){
         res.status(500).json({message:error.message})
