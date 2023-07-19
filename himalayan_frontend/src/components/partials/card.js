a8f0a120e4668e244a256d44f5365bfb422f029e
koshish6969 <koshishshrestha74@gmail.com>
2023-07-19 23:31:34 +0545

    Pull From Bijen Night

--- himalayan_frontend/src/components/partials/card.js
+++ himalayan_frontend/src/components/partials/card.js
@@ -1,20 +1,30 @@
-import React from 'react';
-const ForumCard = ({ title,upvote,downvote, author, subforum, numComments, score }) => {
+import React, { useState } from 'react';
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+
+const ForumPostCard = ({ title,  author, subforum, numComments, score }) => {
+  const [voteCount, setVoteCount] = useState(score);
+  const [isUpvoted, setIsUpvoted] = useState(false);
+  const [isDownvoted, setIsDownvoted] = useState(false);
+
+
   return (
-    <div className="forum-card">
-      <h2 className="title">{title}</h2>
-      <div className="info">
-        <span className="author">Posted by {author}</span>
-        <span className="subforum">in {subforum}</span>
-      </div>
-      <div className="stats">
-      <span className="upvote">{upvote} Upvote</span>
-      <span className="downvote">{downvote} Downvote</span>
-        <span className="comments">{numComments} comments</span>
-        <span className="score">{score} score</span>
+    <div className="forum-post-card">
+         <div className="content">
+         <div className="upvote-icon">
+         <ThumbUpIcon/>
+         </div>
+        <h2 className="title">{title}</h2>
+        <div className="info">
+          <span className="author">Posted by {author}</span>
+          <span className="subforum">in {subforum}</span>
+        </div>
+        <div className="stats">
+          <span className="comments">{numComments} comments</span>
+          <span className="score">{score} score</span>
+        </div>
       </div>
     </div>
   );
 };
 
-export default ForumCard;
+export default ForumPostCard;
--- himalayan_frontend/src/pages/home.js
+++ himalayan_frontend/src/pages/home.js
@@ -1,8 +1,32 @@
 import ForumCard from "../components/partials/card";
 export const Home = () => {
+    // const [results, setResults] = useState([]);
+    // const getDetails = async () => {
+    //     let result = await fetch(`http://localhost:5000/bookdetails/${key}`);
+    //     result = await result.json();
+    //     if (result) {
+    //       setResults(result);
+    //     }
+    //   };
+
   return (
     <div className="home">
-      
+    <ForumCard title="Hello"
+         upvote={20}
+          score={10}
+          author="Bijen"
+          numComments={20}
+          subforum="Ward07" ></ForumCard>
+   {/* {getDetails.map((details, index) => (
+         <>
+        <ForumCard title={details.content}
+         upvote={details.upvote}
+          downvote={details.downvote}
+          author={details.author}
+          subforum={details.subforum} ></ForumCard>
+        </>
+   )) } */}
+
     </div>
   );
 };
