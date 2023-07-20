527e0c44038b6466d718b17cf363fa33751799d2
AnkitShrestha-Xenserv <aankitshrestha@gmail.com>
2023-07-20 19:50:58 +0545

    conflicts

--- Himalayan Backend/routes/postRoutes.js
+++ Himalayan Backend/routes/postRoutes.js
@@ -3,21 +3,18 @@ const router=express.Router();
 //const {Post}=require("../models/postSchema");
 const {createPost,getPosts,getWardPosts, getPostById} = require("../controllers/postController")
 
-// [/api/post] get all the existing posts
-router.get('/',getPosts)
 
-<<<<<<< HEAD
-// [/api/post/ward/:wardNumber] get all the posts of a particular ward
-=======
-//get post by id
-router.get('/:id',getPostById)
 //router.get('/:id',getPost)
->>>>>>> efff67deabda517d7bc69ce5a04544d3f9768c9b
 router.get('/ward/:wardNumber',getWardPosts)
 
 // [/api/post/create] Create a post
 router.post('/create',createPost)
 
-//router.post("/overdue",sendHigher)
+// [/api/post] get all the existing posts
+router.get('/',getPosts)
+
+// [/api/post/ward/:wardNumber] get all the posts of a particular ward
+router.get('/:id',getPostById)
+
 
 module.exports=router
