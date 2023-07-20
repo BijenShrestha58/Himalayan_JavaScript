21831fc6b3b50d438becda8fcd5ab99f6f9fe40b
Bijen <gr8bijen@gmail.com>
2023-07-21 00:58:02 +0545

    forums progress

--- himalayan_frontend/src/components/modules/addpost.js
+++ himalayan_frontend/src/components/modules/addpost.js
@@ -1,65 +1,39 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import axios from "axios";
-const AddPost = (props) => {
-  const [result, setResult] = useState("");
-  const author = localStorage.getItem("userId");
-  const [postData, setPostData] = useState("");
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    // Implement the logic to submit the new post (e.g., send data to the server)
-    // console.log("New Post Content:", post);x                         
-    // Clear the input field after submitting
+import { useState } from "react";
+import DialogBox from "../common/dialogbox";
 
-    // console.log(postData);
-    // axios
-    //   .post("https://192.168.54.30:3000/api/post/create", postData)
-    //   .then((response) => {
-    //     //setPostData(response.data);
-    //     console.log(response.data);
-    //   })
-    //   .catch((error) => {
-    //     console.error("Error fetching data:", error.message);
-    //   });
-
-    const result = await fetch("https://192.168.54.34:3000/api/post/create", {
-      method: "POST",
-      body: JSON.stringify(postData),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    const data = await result.json();
-    console.log(data);
-  };
-
-  const handleChange = (event) => {
-    setPostData(event.target.value);
-  };
-
-  return (
-    <div className="add-post-container">
-      <textarea
-        className="post-input"
-        placeholder="What's happening?"
-        value={postData}
-        onChange={handleChange}
-
-        /*
-        {(e) => {
-          setPostData({
-            ...postData,
-            [e.target.content]: e.target.value,
-          });
-        }}
-      */
-      />
-      <button className="post-button" onClick={handleSubmit}>
-        Post
-      </button>
-    </div>
-  );
-};
-
-export default AddPost;
+export const PostForm  =(props)=>{
+    const [post,setPost] = useState({ wardNumber:'', content: '', author:''});
+    const submitForm=async (e)=>{
+        e.preventDefault();
+        // console.log(localuserId)
+        await props.submit(post)
+    }
+    const localWardNumber=localStorage.getItem('wardNumber');
+    const localUserId=localStorage.getItem('userId')
+    console.log(localUserId)
+    console.log(localWardNumber)
+    const formHandler=(e)=>{
+        setPost({
+            ...post,
+            [e.target.name]: e.target.value,
+            wardNumber: localStorage.getItem('wardNumber'),
+            author: localStorage.getItem('userId')
+            // [wardNumber]:localwardNumber,
+            // [author]:localuserId
+            
+        })
+    }
+    return (
+    <>
+    {props.open &&
+    <DialogBox open={props.open} close={props.close}>
+    <form onSubmit={submitForm}>
+        <input name="content" placeholder="Enter content" required onChange={formHandler}/>
+        {/* <input name="author" placeholder="Enter author" required onChange={formHandler}/> */}
+        <button type="submit">Submit</button>
+    </form>
+    </DialogBox>
+    }
+    </>
+    )
+}
--- himalayan_frontend/src/pages/forums.js
+++ himalayan_frontend/src/pages/forums.js
@@ -1,9 +1,71 @@
-import AddPost from "../components/modules/addpost";
-import { PostDisplay } from "../components/modules/postdisplay";
-import PostCard from "../components/partials/card";
-export const Forums=()=>{
-    return <><AddPost/>
-    <PostCard/>
-    <PostDisplay/>
+import { PostForm } from "../components/modules/addpost"
+import axios from "axios";
+import {useState, useEffect} from "react";
+export const Forums=(props)=>{
+    const localhost="192.168.54.34";
+    const createPost = async(newpost)=>{
+        try{
+         const res = await axios.post(`http://${localhost}:3000/api/officialpost/news`,newpost);
+         await loadPosts();
+        }catch(e){
+         console.log(e);
+        }
+     }
+     const [selectedPost, setSelectedPost] = useState({
+        _id: '',
+        content: '',
+        author:'',
+    });
+
+    const [posts,setPosts]=useState([]);
+    
+    useEffect(()=>{
+
+        (async ()=>{
+            await loadPosts();
+            
+        })();
+    },[])
+    
+
+    const loadPosts = async ()=>{
+        const res = await axios.get(`http://${localhost}:3000/api/officialpost/all`);
+        setPosts(res.data);
+    }
+    const loadSelectedPost =async (id)=>{
+        const res = await axios.get(`http://${localhost}:3000/api/officialpost`+id);
+        setSelectedPost(res.data.data);
+    } 
+  
+    const[isVisible,setIsVisible]=useState(false);
+
+    return (
+    <>
+        <section>
+    <h1>Posts</h1>
+    <button onClick={()=>setIsVisible(true)}>Create new post</button>
+    <PostForm submit={createPost} open={isVisible} close={()=>setIsVisible(false)}/>
+
+    <div className={"posts-list"}>
+            <h2>Posts List</h2>
+        <div className="posts">
+            {posts.map((v, key) => (
+                <div className={"card"} key={key}>
+                    <div>{v.content}</div>
+                    <p>{v.author}</p>
+
+                    {localStorage.getItem("userId") === v.author && <>
+                    {/* <button onClick={()=>loadSelectedPost(v._id)}>Edit Post</button>
+                    <button onClick={()=>deletePost(v._id)}>Delete Post</button> */}
+                    </>
+                    }
+                    
+                </div>
+            ))}
+        </div>
+    </div> 
+    
+</section>
     </>
+    )
 }
