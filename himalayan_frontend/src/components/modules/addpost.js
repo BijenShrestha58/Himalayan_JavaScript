66721d7c4f639e90747d65e3c1447517b008257d
koshish6969 <koshishshrestha74@gmail.com>
2023-07-20 07:11:52 +0545

    Bijen Pull

--- himalayan_frontend/src/components/modules/addpost.js
+++ himalayan_frontend/src/components/modules/addpost.js
@@ -2,18 +2,24 @@ import React from "react";
 import { useState } from "react";
 import axios from "axios";
 const AddPost = () => {
-  const [post, setPost] = useState("");
+  //   const [post, setPost] = useState("");
+  //   const [id, setId] = useState("64b83aaeb2c20437ded5ae57");
+
+  const [postData, setPostData] = useState({
+    content: "adasdsa",
+    author: "64b83aaeb2c20437ded5ae57",
+  });
 
   const handleSubmit = () => {
     // Implement the logic to submit the new post (e.g., send data to the server)
-    console.log("New Post Content:", post);
+    // console.log("New Post Content:", post);
     // Clear the input field after submitting
-    setPost("");
+    // setPost("");
 
     axios
-      .post(`http://192.168.54.34:3000/api/post/create`)
+      .post("http://192.168.54.34:3000/api/post/create", postData)
       .then((response) => {
-        setPost(response.data);
+         setPostData(response.data);
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
@@ -24,8 +30,8 @@ const AddPost = () => {
       <textarea
         className="post-input"
         placeholder="What's happening?"
-        value={post}
-        onChange={(e) => setPost(e.target.value)}
+        // value={post}
+        // onChange={(e) => setPost(e.target.value)}
       />
       <button className="post-button" onClick={handleSubmit}>
         Post
